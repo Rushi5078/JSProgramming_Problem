f44e6f9ebf9b5bb0365ef4619c2d80cdcf54d38b
Rushi5078 <rushikeshb5078@gmail.com>
2021-10-13 17:15:58 +0530

    Add: initial commit

---
+++ FindSumAndAvg.js
@@ -0,0 +1,11 @@
+let num1=(Math.floor(Math.random()*99)+1);
+let num2=(Math.floor(Math.random()*99)+1);
+let num3=(Math.floor(Math.random()*99)+1);
+let num4=(Math.floor(Math.random()*99)+1);
+let num5=(Math.floor(Math.random()*99)+1);
+
+let add=num1+num2+num3+num4+num5;
+console.log("Addition of five random number " + num1 + " + " + num2 + " + " + num3 + " + " + num4 + " + " + num5 + " are = " + add);
+
+let avg=add/5;
+console.log("Average of five number is "+ avg);
---
+++ TwoDiceNumber.js
@@ -0,0 +1,4 @@
+let a=(Math.floor(Math.random()*6)+1);
+let b=(Math.floor(Math.random()*6)+1);
+let add=a+b;
+console.log("Addition of two random dice number " + a + " + " + b + " are = " + add);
